8465313dc84e93a9f101f3b072bd5dff9dd0d655
0PrashantYadav0 <prashantyadav09783@gmail.com>
2024-07-28 18:21:09 +0530

    models: client config, server config ans controller

---
+++ src/models/name.ts
@@ -0,0 +1,6 @@
+export const db = "stack-over-flow-clone"
+export const questionCollection = "questions"
+export const answerCollection = "answers"
+export const commentCollection = "comment"
+export const voteCollection = "votes"
+export const questionAttachmentBucket = "questions-attachement"
